8a17fa2717d8e3aef8274bb33cf4fb890611a56f
Andrew Artiushok <loony.developer@gmail.com>
2019-06-07 07:51:07 +0300

    Added scalable dict for pattern search

--- lib/plugins/paramm.py
+++ lib/plugins/paramm.py
@@ -1,30 +1,53 @@
 from linker import *
 
 
-'''
+"""
 
 This module find and check posible parameter from site
 
-'''
+"""
 
-patterns = ["name="]
-offset = 1
+patterns = [
+    {"start_with": "name=", "end_with": '"', "start_offset": 1, "end_offset": -1}
+]
+
 
 def param_miner(url):
     param_list = []
-    get_example = requests.get(url = url).text
+    get_example = requests.get(url=url).text
     for pattern in patterns:
-        start_index = get_example.find(pattern)
+        start_index = get_example.find(pattern["start_with"])
         end_index = -1
-        while start_index!=-1:
-            end_index = get_example.find('"',start_index + len(pattern)+offset)
-            param_list.append(get_example[start_index+len(pattern)+offset:end_index])
-            start_index = get_example.find(pattern,end_index)
+        while start_index != -1:
+            offset_for_search = (
+                start_index + len(pattern["start_with"]) + pattern["start_offset"]
+            )
+            end_index = get_example.find(pattern["end_with"], offset_for_search)
+            param_list.append(
+                get_example[
+                    start_index
+                    + len(pattern["start_with"])
+                    + pattern["start_offset"] : end_index
+                ]
+            )
+            start_index = get_example.find(pattern["start_with"], end_index)
     return [str(x) for x in param_list]
 
 
-def paramm(endpoint,timeout=30):
-    url = helpers.urlify(endpoint)['URL_FILE']
+def diff_check(example, current):
+    pass
+
+
+def get_clear(url, type="get"):
+    pass
+
+
+def check_param(url, param_list):
+    pass
+
+
+def paramm(endpoint, timeout=30):
+    url = helpers.urlify(endpoint)["URL_FILE"]
     result = param_miner(url)
     # print(url)
-    return {"posible_name" : result}
+    return {"posible_name": result}
